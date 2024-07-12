50a0d03e7aac334525ffc7bbdf721e15dc448b1e
liyinghao2009 <85008169+liyinghao2009@users.noreply.github.com>
2024-07-12 15:46:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,19 +11,4 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-
-# Define My Package
-#git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
-#git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
-#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
